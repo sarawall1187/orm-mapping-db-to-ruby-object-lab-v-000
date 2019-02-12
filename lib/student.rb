d6b1df834ae025de3ed530bfc7aa1c393d7f1252
Sara Walker <sarawall1187@gmail.com>
2019-02-12 19:14:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -54,8 +54,15 @@ class Student
    end
   end
 
-  def self.first_X_students_in_grade_10()
-
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade = ?
+    SQL
+    DB[:conn].execute(sql, x).map do |row|
+     self.new_from_db(row)
+   end
   end
 
   def self.first_student_in_grade_10
