178b930632ae8fb88e4b4dbba1717e245de2a4d3
Sara Walker <sarawall1187@gmail.com>
2019-02-12 19:24:42 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -56,12 +56,13 @@ class Student
   end
 
   def self.first_X_students_in_grade_10(x)
+      binding.pry
     sql = <<-SQL
     SELECT *
     FROM students
     WHERE grade = ?
     SQL
-    binding.pry
+    
     DB[:conn].execute(sql, x).map do |row|
      self.new_from_db(row)
 
