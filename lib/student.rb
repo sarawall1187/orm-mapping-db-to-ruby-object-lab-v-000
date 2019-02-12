36503d4429ef1a21260ece0ed0f2322c36b9fc66
Sara Walker <sarawall1187@gmail.com>
2019-02-12 19:14:49 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,7 +58,7 @@ class Student
     sql = <<-SQL
     SELECT *
     FROM students
-    WHERE grade = ?
+    WHERE grade = 10
     SQL
     DB[:conn].execute(sql, x).map do |row|
      self.new_from_db(row)
